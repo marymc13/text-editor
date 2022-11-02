45f85819c995eb3e9b928ae0ce896c0efc7d91ee
Mary Paul <marymc135@hotmail.com>
2022-11-01 21:10:34 -0400

    packages

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,4 +1,5 @@
 import { openDB } from 'idb';
+import 'regenerator-runtime/runtime';
 
 const initdb = async () =>
   openDB('jate', 1, {
@@ -12,10 +13,23 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+//Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => console.error('putDb not implemented');
+// console.log('put to the database');
+
+// const jateDb = await openDB('jate', 1);
+
+// const tx = jateDb.transaction('jate', 'readwrite');
+
+// const store = tx.objectStore('jate');
+
+// const request = store.put({ id: 1, value: content });
+// //results
+// const result = await request;
+//   console.log('data saved to database', result);
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
+console.log('GET from the database');
 
-initdb();
+
--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -8,7 +8,7 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: 'production',
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js'
@@ -18,12 +18,53 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE',
+        inject: true,
+      }),
+      new WebpackPwaManifest({
+        name: 'Jate-code-editor-Application',
+        short_name: 'JATE',
+        description: 'Code editor that can be install and used even offline!',
+        background_color: '#808080',
+        theme_color: '#808080',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          }
+        ],
+      }),
+      //workbox plugins
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\m$js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+              presets: [
+                '@babel/preset-env', { targets: "defaults" }
+              ]
+            }
+          }
+        }
       ],
     },
   };
